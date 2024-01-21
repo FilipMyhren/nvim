f99ef767a82ab55371c5a1cf76d64fcf3bcef8cc
Filip Myhren <filip.myhren@ingka.ikea.com>
2024-01-21 21:58:03 +0100

    Removed macDown

--- init.lua
+++ init.lua
@@ -139,13 +139,12 @@ require('lazy').setup({
   },
 
   { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+   -- "lukas-reineke/indent-blankline.nvim",
+    "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    main = "ibl",
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
@@ -183,9 +182,6 @@ require('lazy').setup({
 
   {
     'github/copilot.vim'
-  },
-  {
-    'hashrocket/vim-macdown'
   },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
